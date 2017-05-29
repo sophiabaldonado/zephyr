ae362175a1915241a76e319927fe4234c690a7da
bjorn <tie.372@gmail.com>
2017-05-29 11:59:48 -0700

    Basic physics;

--- main.lua
+++ main.lua
@@ -2,15 +2,29 @@ local simple = require 'shaders.simple'
 
 function lovr.load()
   level = lovr.graphics.newModel('art/level.obj')
-  balloon = lovr.graphics.newModel('art/mobile_balloon.obj', 'art/mobile_DIFF.png')
+  world = lovr.physics.newWorld()
+  balloon = {
+    model = lovr.graphics.newModel('art/mobile_balloon.obj', 'art/mobile_DIFF.png'),
+    collider = world:newBoxCollider(.075)
+  }
+  balloon.collider:setPosition(0, .25, -1)
+  balloon.collider:getShapeList()[1]:setPosition(0, -.2, 0)
+  balloon.collider:setGravityIgnored(true)
   windmillBlades = lovr.graphics.newModel('art/windmill-blades.obj', 'art/windmill-blades_texture0.png')
   lovr.graphics.setShader(simple())
 end
 
+function lovr.update(dt)
+  world:update(dt)
+end
+
 function lovr.draw()
   local x, y, z = 0, -.75, -2
-  local angle = lovr.timer.getTime() * 6
+  local angle = lovr.timer.getTime() * 2
   level:draw(x, y, z, .03)
-  balloon:draw(x, math.sin(angle * .15) * .03, -1, .005)
   windmillBlades:draw(.5, 0, -1.3, .2, angle, 0, 0, 1)
+  x, y, z = balloon.collider:getPosition()
+  balloon.model:draw(x, y, z, .005, balloon.collider:getOrientation())
+  local sx, sy, sz = balloon.collider:getShapeList()[1]:getPosition()
+  lovr.graphics.cube('fill', x + sx, y + sy, z + sz, .075)
 end
